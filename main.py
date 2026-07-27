f9f89ef39beb35bb014aecb85eef112f63ba10ad
kiritsinhchauhan760-wq <kiritsinhchauhan760@gmail.com>
2026-07-27 15:07:14 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+
+TOKEN =8913869539:AAEXraTRAAu9qXBkiC8mLYV_Il2Gr0IWw3E
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        [InlineKeyboardButton("💎 Premium", callback_data="premium")]
+    ]
+    await update.message.reply_text(
+        "👋 Welcome!",
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
+
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+    await query.edit_message_text("💎 Premium Price: ₹199")
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button))
+app.run_polling()
